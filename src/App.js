bd4de7d622fe66e32f497bbad4a57878f9aace90
Jack Staples <jackstaplesjcrs@gmail.com>
2020-06-21 12:44:12 -0700

    Add material ui, Lifecycle player value controller by app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import LifecycleDropdown from "./components/LifecycleDropdown";
 
 function App() {
+  const [selectedPlayer, setSelectedPlayer] = useState("");
   return (
     <div className="App">
       <h1>React Lifecycle Testing with Enzyme</h1>
+      <LifecycleDropdown
+        value={selectedPlayer}
+        onChange={(event) => {
+          if (event?.target?.value) setSelectedPlayer(event?.target?.value);
+        }}
+      />
     </div>
   );
 }
---
+++ src/components/LifecycleDropdown.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Select } from "@material-ui/core";
+
+export default class LifecycleDropdown extends Component {
+  render() {
+    return <Select />;
+  }
+}
